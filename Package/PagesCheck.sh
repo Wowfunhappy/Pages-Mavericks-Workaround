d96f218486feace56a95249a11bc548cde585a6b
Wowfunhappy <Wowfunhappy@gmail.com>
2021-03-20 13:27:11 -0400

    Add package files

---
+++ Package/PagesCheck.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+[[ `defaults read /Applications/iWork\ \'09/Pages.app/Contents/Info CFBundleVersion` -ge "1048" ]]
---
+++ Package/postinstall.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./insert_dylib --inplace --all-yes @executable_path/../Frameworks/PagesMavericksWorkarounds.dylib /Applications/iWork\ \'09/Pages.app/Contents/MacOS/Pages
